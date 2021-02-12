b5835e2a4f2f8b70f99700f9a32aa99ea3c3320b
Ben McGrath <brmc14@gmail.com>
2021-02-12 16:22:10 -0500

    WIP: messing around with jest/mocks testing redux actions

--- client/src/tests/actions/auth.test.js
+++ client/src/tests/actions/auth.test.js
@@ -1,14 +1,31 @@
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
 import { loadUser } from '../../actions/auth';
 import { USER_LOADED } from '../../actions/types';
+import mockLocalStorage from '../__mocks__/mockLocalStorage';
+
+jest.mock('axios');
+const mockStore = configureMockStore([thunk]);
+window.localStorage = mockLocalStorage;
 
 test('should create an action to load a user', () => {
   const user = {
     name: 'tom brady',
     email: 'theGOAT@gmail.com',
+    avatar: '//www.tombradydrunkatparade.com',
+    date: '2021-02-10',
   };
+
+  axios.get.mockResolvedValue({
+    data: [user],
+  });
+
+  mockStore.dispatch(loadUser());
+
   const expectedAction = {
     type: USER_LOADED,
     payload: user,
   };
-  expectedAction(loadUser());
+  expect(store.getActions()).toEqual(expectedAction);
 });
